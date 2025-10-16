f08288bf5b6356f8f4b651a8dd577b2602432831
Varshini-108 <18varshini06@gmail.com>
2025-10-16 21:34:40 +0530

    Create assignment1.py
    This is my first Tutdude project

---
+++ assignment1.py
@@ -0,0 +1,31 @@
+mathematical operations
+a=input("enter a: ")
+a=int(a)
+b=input("enter b: ")
+b=int(b)
+c=a+b
+c=int(c)
+print('addition: ', c)
+c=a-b
+c=int(c)
+print('subtraction: ', c)
+c=a*b
+c=int(c)
+print('multiplication: ', c)
+c=a/b
+c=int(c)
+print('division: ', c)
+
+
+
+
+
+
+
+
+
+#greeting
+firstname=str(input("enter first name: "))
+lastname=str(input("enter last name: "))
+name=firstname+" "+lastname +"!"
+print("hello,", (name),"welcome to the python program.")
